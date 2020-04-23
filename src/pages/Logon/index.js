c619ee96a753733e4bb0a3d31413a7c4c8d4f506
Tiago Aguiar <tyago_tka48@hotmail.com>
2020-04-23 00:56:14 -0300

    Criado a tela de login

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FiLogIn } from 'react-icons/fi'
+
+import './Logon.css'
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
+
+
+function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero"/>
+
+        <form>
+          <h1>Faça seu Logon</h1>
+
+          <input type="text" placeholder="Sua ID" />
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041" />  
+            Não tenho cadastro
+          </a>
+        </form>
+
+      </section>
+
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  )
+}
+
+export default Logon
